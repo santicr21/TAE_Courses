758c5afdc651cb91441a4758638784af04b11fbf
Santiago Caicedo <santiago.caicedo@xpo.com>
2024-04-04 07:01:18 -0500

    Added: New conditions to 0 and 1

--- Agile Testing/Exercise1/is_prime.py
+++ Agile Testing/Exercise1/is_prime.py
@@ -1,7 +1,9 @@
 def is_prime(n):
     i = 2
-    while (i <= n ** (1/2)):
-        if (n % i == 0):
-            return False
-        i += 1
-    return True
+    ans = True if n > 1 else False
+    if n != 0:
+        while (i <= n ** (1/2)):
+            if (n % i == 0):
+                ans = False
+            i += 1
+    return ans
